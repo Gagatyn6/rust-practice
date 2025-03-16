8d08363eed5b9de825ad843bf819edbdde13321b
Gagatyn6 <alexeyartamonov05@gmail.com>
2025-03-16 18:41:49 +0100

    Update hw01.rs

--- src/homework/hw01.rs
+++ src/homework/hw01.rs
@@ -1,4 +1,25 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+const WIDTH: usize = 20; 
+const HEIGHT: usize = 10; 
+
+fn main() {
+  
+    for i in 0..WIDTH {
+        print!("*");
+    }
+    println!();  
+
+    
+    for _ in 1..HEIGHT - 1 {
+        print!("*");
+        for _ in 1..WIDTH - 1 {
+            print!(" ");
+        }
+        println!("*");
+    }
+
+    
+    for i in 0..WIDTH {
+        print!("*");
+    }
+    println!();  
 }
